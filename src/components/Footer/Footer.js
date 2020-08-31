1f725b98e7c791d823c9cb581a6d3646049913a4
2MarketingHub <64784186+2marketingHub@users.noreply.github.com>
2020-08-31 22:20:40 +0300

    header footer phone number update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -23,6 +23,10 @@ const Footer = () => {
         }
         page(id: "global-information", idType: URI) {
           globalFields {
+            phoneNumber {
+              label
+              url
+            }
             logoBlack {
               sourceUrl
               altText
@@ -42,6 +46,8 @@ const Footer = () => {
 
   const logo = data.wpgraphql.page.globalFields.logoBlack
   const menu = data.wpgraphql.menu.menuItems.edges
+  const phone = data.wpgraphql.page.globalFields.phoneNumber
+  
 
   return (
     <>
@@ -79,7 +85,7 @@ const Footer = () => {
 
         <Container>
           <Row className={styles.Row}>
-            <Col md={6}>
+            <Col md={7}>
               <div className={styles.FollowUs}>
                 <h2>Follow Us</h2>
                 <ul>
@@ -135,11 +141,33 @@ const Footer = () => {
                     </svg>
                   </li>
                 </ul>
+
+                <div className={styles.Phone}>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="phone-alt"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      fill="#ffffff"
+                      d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"
+                    ></path>
+                  </svg>
+                  <span>
+                    <a href={phone.url}>{phone.label}</a>
+                  </span>
+                </div>
               </div>
             </Col>
 
-            <Col md={6}>
-              <span className={styles.rr}>© {new Date().getFullYear()} All Rights Reserved</span>
+            <Col md={5}>
+              <span className={styles.rr}>
+                © {new Date().getFullYear()} All Rights Reserved
+              </span>
             </Col>
           </Row>
         </Container>
